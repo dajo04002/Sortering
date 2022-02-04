7aed546265a543e9715af79b86a8f530c97887db
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-02-04 15:52:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sortering_uppg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+           
+
+            int[] arr = new int[100];
+
+            for (int i = 0; i < 99; i++)
+            {
+                arr[i] = rnd.Next(1,100);
+            }
+
+            foreach (int e in arr)
+            {
+               Console.WriteLine(arr[e]); 
+            }
+            
+
+            
+        }
+    }
+}
